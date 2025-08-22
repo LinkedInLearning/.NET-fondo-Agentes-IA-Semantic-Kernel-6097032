366f9f2df32353e172358f9178cba03a5ac7f75c
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2025-08-22 02:03:51 -0600

    02_05i

--- MyFirstSKApp/Program.cs
+++ MyFirstSKApp/Program.cs
@@ -1,32 +1,14 @@
 ﻿using Microsoft.Extensions.AI;
 using Microsoft.SemanticKernel;
 
-var modelId = "qwen3:4b";
-var apiKey = "none";
+var modelId = "gpt-4o";
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
 var kernelBuilder = Kernel.CreateBuilder();
 
-kernelBuilder.AddOpenAIChatClient(modelId, new Uri("http://localhost:11434/v1"), apiKey);
+kernelBuilder.AddOpenAIChatClient(modelId, apiKey);
 
 var kernel = kernelBuilder.Build();
 
 var chatClient = kernel.GetRequiredService<IChatClient>();
 
 var prompt = "Cuáles son las ciudades más grandes del mundo?";
-
-Console.WriteLine("Pensando...");
-
-bool hasFinishedThinking = false;
-
-await foreach (var item in chatClient.GetStreamingResponseAsync(prompt))
-{
-    if (item.Text == "</think>")
-    {
-        hasFinishedThinking = true;
-        continue;
-    }
-
-    if (hasFinishedThinking)
-    {
-        Console.Write(item);
-    }
-}
