dce82174172d8d915b0f1d65f4d8381841e420bc
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2025-08-22 02:26:40 -0600

    02_05f

--- MyFirstSKApp/Program.cs
+++ MyFirstSKApp/Program.cs
@@ -11,4 +11,23 @@ var kernel = kernelBuilder.Build();
 
 var chatClient = kernel.GetRequiredService<IChatClient>();
 
-var prompt = "Cuáles son las ciudades más grandes del mundo?";
+var prompt = """
+    Cuáles son las ciudades más grandes del mundo?
+    Regresa un arreglo en JSON, donde cada elemento tiene
+    la siguiente estructura: Position, Name, Country, Population
+    """;
+
+var chatOptions = new ChatOptions()
+{
+    Temperature = 0.1f,
+    ResponseFormat = ChatResponseFormat.Json
+};
+
+var response = await chatClient.GetResponseAsync<IEnumerable<City>>(prompt, chatOptions);
+
+foreach (var city in response.Result)
+{
+    Console.WriteLine(city);
+}
+
+public record City(int Position, string Name, string Country, string Population);
