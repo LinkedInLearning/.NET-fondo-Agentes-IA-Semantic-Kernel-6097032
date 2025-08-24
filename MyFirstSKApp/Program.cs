f109dc0299273145e5975e66afe1064879b62f19
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2025-08-24 00:59:21 -0600

    03_02f

--- MyFirstSKApp/Program.cs
+++ MyFirstSKApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 var modelId = "gpt-4o";
 var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
@@ -15,12 +16,16 @@ kernelBuilder.AddOpenAIChatCompletion(modelId, apiKey)
 
 var kernel = kernelBuilder.Build();
 
+var settings = new OpenAIPromptExecutionSettings() { Temperature = 0.7f, MaxTokens = 1000 };
+
+var kernelArgs = new KernelArguments(settings);
+
 while (true)
 {
     Console.Write("Prompt: ");
     var prompt = Console.ReadLine();
 
-    var result = await kernel.InvokePromptAsync(prompt);
+    var result = await kernel.InvokePromptAsync(prompt, kernelArgs);
 
     Console.WriteLine($"\n{result.GetValue<string>()}\n");
 }
