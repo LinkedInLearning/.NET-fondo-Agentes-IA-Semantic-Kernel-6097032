be47359e2526b487c1082790bbaa77bb795f1ff7
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2025-08-26 11:02:23 -0600

    07_03f

--- FabuRobotics.AIAgent/Program.cs
+++ FabuRobotics.AIAgent/Program.cs
@@ -1,18 +1,30 @@
 using FabuRobotics.AIAgent.Plugins;
 using FabuRobotics.AIAgent.Services;
+using Microsoft.KernelMemory;
 using Microsoft.SemanticKernel;
 var builder = WebApplication.CreateBuilder(args);
 
 var apiKey = builder.Configuration.GetValue<string>("FabuRobotics:ApiKey");
 var modelId = builder.Configuration.GetValue<string>("FabuRobotics:ModelId");
-
+var qdrantUrl = "http://localhost:6333";
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddKernelMemory<MemoryServerless>(kernelBuilder =>
+{
+    kernelBuilder.WithOpenAIDefaults(apiKey!)
+                 .WithQdrantMemoryDb(qdrantUrl);
+}, new KernelMemoryBuilderBuildOptions()
+{
+    AllowMixingVolatileAndPersistentData = true
+});
+
 builder.Services.AddKernel()
                 .AddOpenAIChatCompletion(modelId!, apiKey!)
                 .Plugins
                     .AddFromType<InventoryPlugin>()
-                    .AddFromType<ProductionPlugin>();
+                    .AddFromType<ProductionPlugin>()
+                    .AddFromType<MemoryPlugin>();
 builder.Services.AddScoped<AgentService>();
 builder.Services.AddMemoryCache();
 
